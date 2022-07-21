610f9f629f5b85582c4c566d718ccab03a18f222
Talha Ansari <talhaansari491@gmail.com>
2022-07-22 00:46:55 +0530

    18.js

--- javascript basic/18.js
+++
@@ -1,8 +0,0 @@
-// Write a function that takes a number as argument
-// Round the number to the 2nd digit after the comma
-// Return the rounded number
-
-function myFunction(a) {
-    return Math.round((a + Number.EPSILON) * 100) / 100
-  }
-  
