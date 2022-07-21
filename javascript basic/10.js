d2107ee5f7d213c385d5e1e48d28b0e50a66e2ca
Talha Ansari <talhaansari491@gmail.com>
2022-07-21 21:20:52 +0530

    Return the percentage of a number

---
+++ javascript basic/10.js
@@ -0,0 +1,6 @@
+// Write a function that takes an object with the properties number and percentage as argument
+// Return the given percentage of the number
+
+function myFunction({ a, b}) {
+    return a * b / 100
+  }
