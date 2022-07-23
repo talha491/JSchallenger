d6c056113789c57f224f7ba7d4c8e4741de27dea
Talha Ansari <talhaansari491@gmail.com>
2022-07-23 14:32:34 +0530

    Check if property exists in object

---
+++ Javascript objects/04.js
@@ -0,0 +1,9 @@
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if the object has a property with key 'b'
+// Return false otherwise
+// Tipp: test case 3 is a bit tricky because the value of property 'z' is undefined
+// But the property itself exists
+
+function myFunction(a, b) {
+  return b in a;
+}
