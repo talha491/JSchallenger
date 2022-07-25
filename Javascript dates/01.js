776b01c666cb246db041b7ab1e433974a324c073
Talha Ansari <talhaansari491@gmail.com>
2022-07-26 01:23:14 +0530

    Check if two dates are equal

---
+++ Javascript dates/01.js
@@ -0,0 +1,8 @@
+// Sounds easy, but you need to know the trick
+// Write a function that takes two date instances as arguments
+// It should return true if the dates are equal
+// It should return false otherwise
+
+function myFunction(a, b) {
+  return a.getTime() === b.getTime();
+}
